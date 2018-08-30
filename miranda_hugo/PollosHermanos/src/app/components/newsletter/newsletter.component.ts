9d611fe309263c9569e8794b8e2bfeaca3847ced
Miranda <hugo.miranda@accenture.com>
2018-08-30 15:01:20 -0500

    Hugo Miranda (08/30/18) - Newsletter, Footer and corrections.

--- miranda_hugo/PollosHermanos/src/app/components/newsletter/newsletter.component.ts
+++ miranda_hugo/PollosHermanos/src/app/components/newsletter/newsletter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-newsletter',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsletterComponent implements OnInit {
 
+  @ViewChild ("emailInput",{read: ElementRef}) emailInput: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  subscribedAlert()
+  {
+    let userEmail : string = this.emailInput.nativeElement.value
+    // if(userEmail && userEmail.indexOf('@')>-1 && userEmail.indexOf('.')>-1 ){
+    //   alert('Subscribed!');
+    // }
+    if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail)){
+      alert('Subscribed!');
+    }
+    else{
+      alert('Enter a valid email');
+    }
+    
   }
 
 }
