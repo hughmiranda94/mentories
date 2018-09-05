22d4e9e06aa9ad0dc25b0093257371d620ce32b8
Miranda <hugo.miranda@accenture.com>
2018-09-05 15:23:02 -0500

    Hugo Miranda (09/05/2018) - Oltiva Energy Second Page

--- miranda_hugo/Oltiva Energy/main.js
+++ miranda_hugo/Oltiva Energy/main.js
@@ -1,3 +1,5 @@
+//index.html
+
 let nav = document.getElementById("nav");
 let main = document.getElementById("main");
 let navPosition;
@@ -11,12 +13,11 @@ function adjustScreen() {
   if (mobileWidthMatch.matches) {
     navToggle = true;
     displayNav();
-  }
-  else
-  {
+  } else {
     navToggle = false;
     displayNav();
   }
+
 }
 
 function displayNav() {
@@ -68,7 +69,7 @@ function openNavMobile() {
   nav.style.transform = 'translateX(0%)';
   nav.style.height = window.getComputedStyle(main, null).getPropertyValue('height');
   console.log(window.getComputedStyle(main, null).getPropertyValue('height'));
-  
+
   navToggle = true;
 }
 
@@ -93,5 +94,117 @@ function closeNavMobile() {
   nav.style.width = '80%';
   nav.style.transform = 'translateX(-100%)';
   navToggle = false;
+}
 
+//detail.html
+
+let thermostat = document.getElementById("thermostat");
+let efficiency = document.getElementById("efficiency");
+let homeSystem = document.getElementById("home-system");
+let co2alarm = document.getElementById("co2alarm");
+let viewMoreButton = document.getElementById("view-more");
+let windowWidthMatch1200;
+let windowWidthMatch900;
+let windowWidthMatch750;
+let windowWidthMatch600;
+let detailHeader = document.getElementById("detail-header");
+let navUL = document.getElementById("nav-ul");
+let helpCenterNav = document.getElementById("help-center-nav");
+let headerToggle;
+let navULToggle;
+
+function adjustDetailBody() {
+  windowWidthMatch1200 = window.matchMedia("(max-width: 1200px)");
+  windowWidthMatch900 = window.matchMedia("(max-width: 900px)");
+  windowWidthMatch750 = window.matchMedia("(max-width: 750px)");
+  windowWidthMatch600 = window.matchMedia("(max-width: 600px)");
+
+  viewMoreOfferCards();
+  viewLessOfferCards();
+
+  windowWidthMatch600.matches ? hideHeader() : showHeader();
+  windowWidthMatch750.matches ? hideNavUL() : showNavUL();
+}
+
+function viewButton() {
+
+  windowWidthMatch1200 = window.matchMedia("(max-width: 1200px)");
+  windowWidthMatch900 = window.matchMedia("(max-width: 900px)");
+  windowWidthMatch600 = window.matchMedia("(max-width: 600px)");
+
+
+  if (windowWidthMatch1200.matches) {
+    if (viewMoreButton.innerText == 'View More') {
+      viewMoreOfferCards();
+    } else {
+      viewLessOfferCards();
+    }
+  }
+}
+
+function viewMoreOfferCards() {
+  thermostat.style.display = 'flex';
+  efficiency.style.display = 'flex';
+  homeSystem.style.display = 'flex';
+  co2alarm.style.display = 'flex';
+  viewMoreButton.innerText = 'View Less';
+}
+
+function viewLessOfferCards() {
+
+  if (windowWidthMatch600.matches) {
+    efficiency.style.display = 'none';
+    homeSystem.style.display = 'none';
+    co2alarm.style.display = 'none';
+  } else if (windowWidthMatch900.matches) {
+    homeSystem.style.display = 'none';
+    co2alarm.style.display = 'none';
+
+  } else if (windowWidthMatch1200.matches) {
+    co2alarm.style.display = 'none';
+  }
+
+  viewMoreButton.innerText = 'View More';
+
+}
+
+window.matchMedia("(max-width: 600px)").matches ? headerToggle = false : headerToggle = true;
+window.matchMedia("(max-width: 750px)").matches ? navULToggle = false : navULToggle = true;
+
+function detailMenuButton() {
+
+  if(window.matchMedia("(max-width: 600px)").matches){
+    headerToggle ? hideHeader() : showHeader();
+  }   
+  navULToggle ? hideNavUL() : showNavUL(); 
+
+}
+
+function showHeader() {
+
+  detailHeader.style.display = 'flex';
+  headerToggle = true;
+}
+
+function hideHeader() {
+
+  detailHeader.style.display = 'none';
+  headerToggle = false;
+}
+
+function showNavUL() {
+    navUL.style.display = 'flex';
+    if(window.matchMedia("(max-width: 750px)").matches){      
+        helpCenterNav.style.display = 'block';
+    }
+    else{
+      helpCenterNav.style.display = 'none';
+    }
+    navULToggle = true;
+}
+
+function hideNavUL() {
+    navUL.style.display = 'none';
+    helpCenterNav.style.display = 'none';
+    navULToggle = false;
 }
